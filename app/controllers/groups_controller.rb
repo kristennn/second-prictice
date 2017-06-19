d08b7fbb97b60bc5b120c2436710c17351f76db6
郑笑飞“git config --global user.email 13949836783@163.com” <zhengxiaofei@zhengxiaofeideMacBook-Air.local>
2017-06-19 15:52:11 +0800

    add helper and partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-before_action :authenticate_user!, :only => [:new, :create, :edit, :destroy, :update]
-
+before_action :authenticate_user!, :only => [:new, :create, :edit, :destroy, :update, :join, :quit]
+before_action :find_group_and_check_permission, :only => [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -68,6 +68,13 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :destroy, :up
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission"
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
