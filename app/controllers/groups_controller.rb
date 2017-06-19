6ead3e9dce6f001527f30477acd646115ed06700
郑笑飞“git config --global user.email 13949836783@163.com” <zhengxiaofei@zhengxiaofeideMacBook-Air.local>
2017-06-19 10:46:03 +0800

    group&user relationship

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,6 +19,7 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :destroy, :up
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
     if @group.save
       redirect_to groups_path
     else
